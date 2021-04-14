a3f029d1a5863f9945fbd9c22f1253a2dfdfc361
Rendow <rendow33@gmail.com>
2021-04-14 15:38:06 +0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import './index.css';
 import store from './Redux/redux-store';
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from 'react-redux';
 
 
-    ReactDOM.render(
+ReactDOM.render(
         <React.StrictMode>
-            <BrowserRouter>
+            <HashRouter>
                 <Provider store={store}>
                 <App/>
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
