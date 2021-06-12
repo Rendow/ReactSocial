33117fac52bd758c082de3a3e338422058a9e01b
Rendow <rendow33@gmail.com>
2021-06-12 18:07:42 +0300

    small redesign

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,8 +2,7 @@ import React, {useEffect} from "react";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import s from './Profile.module.css'
-import { ProfileType } from "../../redux/propfile-reducer";
-import {Redirect} from "react-router-dom";
+import {ProfileType} from "../../redux/propfile-reducer";
 
 type PropsType = {
     profile: ProfileType | null
@@ -13,8 +12,9 @@ type PropsType = {
 function Profile(props:PropsType) {
 
     useEffect(() => {
-        document.title = 'Profile'
-    },[])
+        document.title =  props.profile?.fullName ? props.profile?.fullName : 'Profile'
+    },[props.profile?.fullName])
+
     return (
         <div className={s.content}>
             <ProfileInfo profile={props.profile}
--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,7 +3,6 @@ import classes from "./ProfileInfo.module.css";
 import Logo from './../MyPosts/Post/img.webp';
 import {ProfileType} from "../../../redux/propfile-reducer";
 import {Preloader} from "../../common/Preloader/Preloader";
-import { ProfileStatus } from "./ProfileStatus";
 import {ProfileStatusWithHooks} from "./ProfileStatusWithHooks";
 
 type PropsType = {
@@ -23,7 +22,9 @@ function ProfileInfo(props: PropsType) {
             <div className={classes.descriptionBlock}>
                 <div>{img}</div>
                 <div className={classes.textBlock}>
-                    <div className={classes.status}> <ProfileStatusWithHooks  updateStatus= {props.updateStatus} status={props.status} /></div>
+                    <div className={classes.status}>
+                        <div className={classes.name}>{props.profile.fullName} </div>
+                        <ProfileStatusWithHooks  updateStatus= {props.updateStatus} status={props.status} /></div>
                     <div className={classes.description}>Do you know that Falcon 9 is a reusable, two-stage rocket manufactured by SpaceX for the reliable and safe transport of people and
                         payloads into Earth orbit and beyond? Now you know.
                     </div>
